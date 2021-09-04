37ff662234ea7b04a4a9705c6fb0c31f6e5418f9
Reinaldo M Padua <reinaldo.padua@compasso.com.br>
2021-09-03 22:51:56 -0300

    add firebase files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,25 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  // Your web app's Firebase configuration
+
+  firebaseConfig : {
+
+    apiKey: 'AIzaSyAARHwb9UFxkXWDmC4LzgU4FnO0ZeJ1q8s',
+
+    authDomain: 'ilpi-app.firebaseapp.com',
+
+    projectId: 'ilpi-app',
+
+    storageBucket: 'ilpi-app.appspot.com',
+
+    messagingSenderId: '716968935929',
+
+    appId: '1:716968935929:web:5c4cb1b265ceb15d7c3d28'
+  }
+
 };
 
 /*
